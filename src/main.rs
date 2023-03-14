f88a80fcaad0ca6831a893b6f9566b075ac3395a
qbuus <jkocz867@gmail.com>
2023-03-14 22:16:41 +0100

    rust initial code commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/* 
+rustfmt [filedir] auto text format
+cargo b = build
+cargo t = test
+cargo c = check 
+cargo r = run build
+cargo new [dirname] = creates rust project
+*/ 
+
 fn main() {
-    println!("Hello, world!");
+    print!("rust!")
 }
